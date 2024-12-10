3bf8fa68b2066a4002ca85887c32c083cc126138
parkjihun22 <eyeful31@naver.com>
2024-12-10 10:59:30 +0900

    feat : 메인변경

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,11 +16,11 @@ const Footer = () => {
 
 					<div className={styles.innerContainer}>
 						<div className={styles.textContainer}>
-							<div className={styles.text1}>시행: ㈜평택브레인시티3피에프브이 ㅣ 시공: ㈜대우건설, 중흥토건㈜</div>
+							<div className={styles.text1}>본사 : 우21556 인천광역시 남동구 미래로 14, 201호 ㅣ 시공: ㈜한양건설 </div>
 							<div className={styles.text1}>※본 사이트의 내용 및 이미지는 소비자의 이해를 돕기 위한 것으로 실제와 다를 수 있습니다.</div>
 							<div className={styles.text1}>대표자명: 박지훈 / eyeful31@naver.com </div>
 
-							<div className={styles.text2}>COPYRIGHTⓒ 평택브레인시티푸르지오 모델하우스 ALL RIGHTS RESERVED.</div>
+							<div className={styles.text2}>COPYRIGHTⓒ 평택 브레인시티 수자인 ALL RIGHTS RESERVED.</div>
 						</div>
 
 						<a href="https://naver.me/G58kVeiB"><IoCall size={25} /> 1533-8848</a>
@@ -33,10 +33,10 @@ const Footer = () => {
 					<div className={styles.innerContainer}>
 
 						<div className={styles.textContainer}>
-							<div className={styles.text1}>시행: ㈜평택브레인시티3피에프브이 ㅣ 시공: ㈜대우건설, 중흥토건㈜</div>
+							<div className={styles.text1}>본사 : 우21556 인천광역시 남동구 미래로 14, 201호 ㅣ 시공: ㈜한양건설</div>
 							<div className={styles.text1}>※본 사이트의 내용 및 이미지는 소비자의 이해를 돕기 위한 것으로 실제와 다를 수 있습니다.</div>
 							<div className={styles.text1}> 대표자명: 박지훈 / eyeful31@naver.com </div>
-							<div className={styles.text2}>COPYRIGHTⓒ 브레인시티푸르지오 모델하우스 ALL RIGHTS RESERVED.</div>
+							<div className={styles.text2}>COPYRIGHTⓒ 평택 브레인시티 수자인 ALL RIGHTS RESERVED.</div>
 						</div>
 					</div>
 				</div>
