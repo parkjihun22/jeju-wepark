cc4606053553a74ffac1f8beb4cd6d33fe35b955
parkjihun22 <eyeful31@naver.com>
2025-03-11 10:31:39 +0900

    백링크해제

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -14,7 +14,7 @@ import MobilePopup from "../../components/MobilePopup/MobilePopup";
 import Popup from "../../components/Popup/Popup";
 import MobileSectionBox from "../../components/MobileSectionBox/MobileSectionBox";
 import InterestPopup from "../../components/InterestPopup/InterestPopup"; // 새 팝업 컴포넌트 import
-import UrlContainer from "../../components/UrlContainer/UrlContainer";
+// import UrlContainer from "../../components/UrlContainer/UrlContainer";
 
 
 import mainImage from "../../assets/Main/Main1.jpg";
@@ -411,7 +411,7 @@ const Main = () => {
           </div> */}
 
           <div className={styles.section5}>
-            <UrlContainer />
+
             <Footer />
           </div>
 
@@ -602,7 +602,6 @@ const Main = () => {
           </div> */}
 
           <div className={styles.section5}>
-            <UrlContainer />
             <Footer />
             <FixIcon />
           </div>
